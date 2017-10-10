7d194c2ce8d5efedbf8d21e2813fc7f95020652a
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-10-09 18:19:46 -0700

    Assign CIDR blocks for subnet and VPC

--- var-vpc.tf
+++ var-vpc.tf
@@ -4,3 +4,30 @@ variable "region" {
 }
 
 data "aws_availability_zones" "available" {}
+
+variable "vpc_cidr" {
+    description = "VPC CIDR block"
+    default = "10.10.0.0/16"
+}
+
+variable "public_cidr" {
+    description = "CIDR blocks for public subnets"
+    default = [
+        "10.10.1.0/24",
+        "10.10.2.0/24",
+        "10.10.3.0/24",
+        "10.10.4.0/24",
+        "10.10.5.0/24"
+    ]
+}
+
+variable "private_cidr" {
+    description = "CIDR blocks for private subnets"
+    default = [
+        "10.10.101.0/24",
+        "10.10.102.0/24",
+        "10.10.103.0/24",
+        "10.10.104.0/24",
+        "10.10.105.0/24"
+    ]
+}
