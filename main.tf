d99be985d27765e5116ff73e5c924392eb50ef41
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-10-09 18:21:52 -0700

    Create provider and VPC configuration

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+    region = "${var.region}"
+}
+
+resource "aws_vpc" "default" {
+    cidr_block = "${var.vpc_cidr}"
+    enable_dns_hostnames = true
+    tags {
+        Name = "magento2"
+        group = "mage-vpc"
+    }
+}
