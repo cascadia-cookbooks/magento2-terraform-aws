1dfc15512624a06514d995dd70947ab4c8e4b618
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-10-10 18:11:18 -0700

    Check that user is logged in to LastPass

--- scripts/lpass-data.sh
+++ scripts/lpass-data.sh
@@ -2,6 +2,16 @@
 
 eval "$(jq -r '@sh "PROJECT=\(.project)"')"
 
+# Check if logged in to LastPass
+if ! lpass status | grep -q "Logged in as " ; then
+    echo " " >&2
+    echo " " >&2
+    echo "Please login to LastPass CLI" >&2
+    echo "EXAMPLE" >&2
+    echo "      $ lpass login username@example.com" >&2
+    exit 1
+fi
+
 LPASS_AWS_ACCESS_KEY=$(lpass show "$PROJECT/AWS Credentials" --access_key)
 LPASS_AWS_SECRET_KEY=$(lpass show "$PROJECT/AWS Credentials" --secret_key)
 
