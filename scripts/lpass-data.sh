3f9371ba059604c4c2f88d78fb8299169008bc3b
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-10-13 12:29:09 -0700

    Format error

--- scripts/lpass-data.sh
+++ scripts/lpass-data.sh
@@ -12,7 +12,7 @@ if ! lpass status | grep -q "Logged in as " ; then
     exit 1
 fi
 
-LPASS_AWS_ACCESS_KEY=$(lpass show "$PROJECT/AWS Credentials" --access_key)
-LPASS_AWS_SECRET_KEY=$(lpass show "$PROJECT/AWS Credentials" --secret_key)
+LPASS_AWS_ACCESS_KEY=$(lpass show "$PROJECT/AWS Credentials" --field=access_key)
+LPASS_AWS_SECRET_KEY=$(lpass show "$PROJECT/AWS Credentials" --field=secret_key)
 
 jq -n --arg access_key "$LPASS_AWS_ACCESS_KEY" --arg secret_key "$LPASS_AWS_SECRET_KEY" '{"access_key":$access_key, "secret_key":$secret_key}'
