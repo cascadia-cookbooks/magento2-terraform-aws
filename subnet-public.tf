5b28d54436e75b794c404c6a82e37a44a1eb4924
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-10-13 12:38:03 -0700

    Route table

--- subnet-public.tf
+++ subnet-public.tf
@@ -4,15 +4,30 @@ resource "aws_internet_gateway" "default" {
 }
 
 # Public subnets
-resrouce "aws_subnet" "public" {
+resource "aws_subnet" "public" {
     count = 1
     vpc_id = "${aws_vpc.default.id}"
     cidr_block = "${var.public_cidr[count.index]}"
     availability_zone = "${data.aws_availability_zones.available.names[count.index]}"
     map_public_ip_on_launch = true
-    depends_on = ["aws_internet_gateway"]
+    depends_on = ["aws_internet_gateway.default"]
     tags {
         Name = "public-${count.index}"
         group = "mage-sub"
     }
+}
+
+# Routing table and association
+resource "aws_route_table" "public" {
+    vpc_id = "${aws_vpc.default.id}"
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = "${aws_internet_gateway.default.id}"
+    }
+}
+
+resource "aws_route_table_association" "public" {
+    count = 1
+    subnet_id = "${element(aws_subnet.public.*.id, count.index)}"
+    route_table_id = "${aws_route_table.public.id}"
 }
