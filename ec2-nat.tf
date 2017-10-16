5498580be6a246f3ecdaa20b2b520e892c6c5540
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-10-15 19:33:48 -0700

    Allocate EIPs

--- ec2-nat.tf
+++ ec2-nat.tf
@@ -10,3 +10,9 @@ resource "aws_instance" "nat" {
     ami = "${data.aws_ami.nat.id}"
     instance_type = "${var.ec2-nat-instance-type}"
 }
+
+resource "aws_eip_association" "nat_eip" {
+    count = "${data.aws_availability_zones.available.names.count}"
+    instance_id = "${element(aws_instance.nat.*.id, count.index)}"
+    allocation_id = "${element(aws_eip.nat.*.id, count.index)}"
+}
