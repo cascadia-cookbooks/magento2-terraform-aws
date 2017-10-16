a976f789ccc896f93984b43f9806ba45b14e478f
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-10-15 19:44:48 -0700

    NAT config

--- ec2-nat.tf
+++ ec2-nat.tf
@@ -9,6 +9,13 @@ resource "aws_instance" "nat" {
     count = "${data.aws_availability_zones.available.names.count}"
     ami = "${data.aws_ami.nat.id}"
     instance_type = "${var.ec2-nat-instance-type}"
+    subnet_id = "${element(aws_subnet.public.*.id, count.index)}"
+    vpc_security_group_ids = ["${aws_security_group.nat.id}"]
+    source_dest_check = false
+    #key_name = "pass" # TODO: dependent on security group update
+    tags = {
+        Name = "magento-nat-${count.index}"
+    }
 }
 
 resource "aws_eip_association" "nat_eip" {
