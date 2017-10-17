6b4813cdcaf1884d2d72fa5679478b96837406d3
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-10-17 16:50:31 -0700

    ELB security group

--- security-groups.tf
+++ security-groups.tf
@@ -65,14 +65,14 @@ resource "aws_security_group" "nat" {
     ingress {
         from_port = 80
         to_port = 80
-        portocol = "tcp"
+        protocol = "tcp"
         cidr_blocks = "${var.private_cidr}"
     }
 
     ingress {
         from_port = 443
         to_port = 443
-        portocol = "tcp"
+        protocol = "tcp"
         cidr_blocks = "${var.private_cidr}"
     }
 
@@ -85,6 +85,39 @@ resource "aws_security_group" "nat" {
 
     tags {
         Name = "magento-nat"
+        group = "mage-sg"
+    }
+}
+
+# ELB security group
+resource "aws_security_group" "elb" {
+    name = "magento-elb"
+    description = "ELB security group"
+    vpc_id = "${aws_vpc.default.id}"
+
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        from_port = 443
+        to_port = 443
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = -1
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags {
+        Name = "magento-elb"
         group = "mage-sg"
     }
 }
