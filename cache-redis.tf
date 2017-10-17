cce8e16f61f1b04f8edd153f5ee0a35594ded6e1
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-10-16 17:38:33 -0700

    ElastiCache subnet group

--- cache-redis.tf
+++ cache-redis.tf
@@ -6,5 +6,12 @@ resource "aws_elasticache_cluster" "redis" {
     num_cache_nodes = 1
     parameter_group_name = "pass"
     security_group_ids = ["${aws_security_group.redis.id}"]
-    subnet_group_name = "pass"
+    subnet_group_name = "${aws_elasticache_subnet_group.default.name}"
+}
+
+# Subnet group
+resource "aws_elasticache_subnet_group" "default" {
+    name = "mage-redis-subnet-group"
+    description = "Subnet group for ElastiCache"
+    subnet_ids = ["${aws_subnet.private.*.id}"]
 }
