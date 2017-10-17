6a2f3eea5f20ace0908c7965088cc89858c8ac64
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-10-17 16:37:56 -0700

    ELB config

---
+++ ec2-elb.tf
@@ -0,0 +1,25 @@
+# Load Balancer
+resource "aws_elb" "app" {
+    name = "pass"
+    subnets = ["${aws_subnet.private.*.id}"]
+    security_groups = #["${aws_security_group.elb.id}"]
+
+    listener {
+        instance_port = "${var.elb-instance-port}"
+        lb_port = "${var.elb-lb-port}"
+        instance_protocol = "${var.elb-instance-protocol}"
+        lb_protocol = "${var.elb-lb-protocol}"
+    }
+
+    health_check {
+        healthy_threshold = "${var.elb-healthy-threshold}"
+        unhealthy_threshold = "${var.elb-unhealthy-threshold}"
+        timeout = "${var.elb-timeout}"
+        target = "${var.elb-target}"
+        interval = "${var.elb-interval}"
+    }
+
+    tags {
+        group = "mage-elb"
+    }
+}
