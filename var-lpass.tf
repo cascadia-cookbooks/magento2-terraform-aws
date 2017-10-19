474f3a5f6881cfa7f5d03e77cb0db97e4ea62923
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-10-19 11:12:02 -0700

    Allow for LastPass project name override

--- var-lpass.tf
+++ var-lpass.tf
@@ -1,7 +1,12 @@
 # Pulls variables from LastPass
 data "external" "lpass" {
     query = {
-        project = "Magento 2 Template"
+        project = "${var.lastpass-project}"
     }
     program = ["bash", "${path.module}/scripts/lpass-data.sh"]
+}
+
+variable "lastpass-project" {
+    description = "Project name in LastPass, aka 'Folder'"
+    default = "Magento 2 Template"
 }
