b06b95d37fadeb4398e2033251083fd0472a2406
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-10-19 11:00:35 -0700

    Define autoscaling group

--- ec2-app.tf
+++ ec2-app.tf
@@ -13,3 +13,35 @@ resource "aws_launch_configuration" "app" {
         create_before_destroy = true
     }
 }
+
+# Application autoscaling group
+resource "aws_autoscaling_group" "app" {
+    name = "${var.asg-app-name}"
+    launch_configuration = "${aws_launch_configuration.app.id}"
+    availability_zones =  ["${data.aws_availability_zones.available.names}"]
+
+    vpc_zone_identifier = ["${aws_subnet.private.*.id}"]
+
+    load_balancers = ["${aws_elb.app.name}"]
+    health_check_type = "ELB"
+    health_check_grace_period = "${var.asg-health-check-grace-period}"
+
+    min_size = "${var.asg-min-size}"
+    max_size = "${var.asg-max-size}"
+
+    lifecycle {
+        create_before_destroy = true
+    }
+
+    tag {
+        key = "Name"
+        value = "${var.asg-app-name}"
+        propagate_at_launch = true
+    }
+
+    tag {
+        key = "group"
+        value = "mage-asg"
+        propagate_at_launch = true
+    }
+}
