ac5400ec3e4704b0434adb7a9559dbc7542b3556
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-10-19 10:26:18 -0700

    Auto scaling group

---
+++ ec2-app.tf
@@ -0,0 +1,15 @@
+# Auto scaling group launch configuration
+resource "aws_launch_configuration" "app" {
+    name = "${var.asg-app-name}"
+    image_id = "${data.aws_ami.rhel.id}"
+    instance_type = "${var.asg-app-instance-type}"
+
+    security_groups = ["pass"]
+    key_name = "pass"
+
+    user_data = "${data.template_file.user_data.rendered}"
+
+    lifecycle {
+        create_before_destroy = true
+    }
+}
