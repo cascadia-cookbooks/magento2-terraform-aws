c7df28b708110e8db3305125f9e67ee51f703714
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-10-18 17:51:31 -0700

    Associate security group

--- efs.tf
+++ efs.tf
@@ -13,5 +13,5 @@ resource "aws_efs_mount_target" "efs-mount" {
     count = "${data.aws_availability_zones.available.names.count}"
     file_system_id = "${aws_efs_file_system.mage-efs.id}"
     subnet_id = "${element(aws_subnet.private.*.id, count.index)}"
-    security_groups = ["pass"]
+    security_groups = ["${aws_security_group.efs.id}"]
 }
