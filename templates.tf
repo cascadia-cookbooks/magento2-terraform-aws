8307875a5e36179f4f2e2d574595e721b04a2c88
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-10-19 10:12:58 -0700

    Compile user data template

---
+++ templates.tf
@@ -0,0 +1,9 @@
+# User-data template
+data "template_file" "user_data" {
+    template = "${file("templates/user_data.tpl")}"
+
+    vars {
+        mysql_endpoint = "${aws_db_instance.mysql.address}"
+        redis_endpoint = "${aws_elasticache_cluster.redis.cache_nodes.0.address}"
+    }
+}
